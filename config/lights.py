242010995f3f17e4d5f616f31632590b7c475532
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2017-04-21 07:55:35 +0200

    Passage to PyCharm

--- config/lights.py
+++ config/lights.py
@@ -1,4 +1,4 @@
-ipAddress = '10.6.19.11'
+ipAddress = '10.6.19.21'
 light_keys = ['label','wavelength', 'color', 'triggerLine','simtheta','port','device']
 WAVELENGTH_TO_COLOR = {
     405: (180, 30, 230),
@@ -11,8 +11,8 @@ WAVELENGTH_TO_COLOR = {
 lights = [
 #    ('ambient', 'Ambient', WAVELENGTH_TO_COLOR['white'], 1<<5),
 #    ('405nm', 405, WAVELENGTH_TO_COLOR[405], 1<<13, 10),
-#     ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<1, 9, 7776, 'deepstar'),
+    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<1, 9, 7776, 'deepstar'),
 #    ('561nm', 561, WAVELENGTH_TO_COLOR[561], 1<<13, 8),
 #    ('DIC', 'DIC', WAVELENGTH_TO_COLOR['white'], 1<<6),
-    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<1),
+#    ('488nm', 488, WAVELENGTH_TO_COLOR[488], 1<<1),
 ]

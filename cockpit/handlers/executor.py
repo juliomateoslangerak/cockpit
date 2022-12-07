050c0422c36279d5d245b2e38d38477c1fc0c3c0
David Miguel Susano Pinto <carandraug+dev@gmail.com>
2022-12-07 21:16:27 +0100

    cockpit.handlers.executor: import Iterable from collections.abc (#834)
    Python 3.10 removed the deprecated aliases to the collections module
    which were kept for Python 2 compatibility.  We no longer support
    Python 2 so just import from collections.abc.
    
    (cherry picked from commit efc04560338c9b410032f5f071597476b2c2916c)

--- cockpit/handlers/executor.py
+++ cockpit/handlers/executor.py
@@ -53,7 +53,7 @@ import typing
 
 import matplotlib.pyplot as plt
 
-import collections
+import collections.abc
 
 from microscope import ElectronicShutteringMode
 
@@ -170,7 +170,7 @@ class ExecutorHandler(DeviceHandler):
             if h in self.analogClients:
                 # update analog state
                 lineHandler = self.analogClients[h]
-                if isinstance(args, collections.Iterable):
+                if isinstance(args, collections.abc.Iterable):
                     # Using an indexed position
                     pos = lineHandler.indexedPosition(*args)
                 else:

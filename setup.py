383cd6e92dc2c7d1a02321a91a0205e07aff76ca
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2023-01-16 11:11:28 +0100

    Changed setup.py to get dev version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setuptools.setup(
         'Pyro4>=4.22',
         'freetype-py',
         'matplotlib',
-        'microscope>=0.5',
+        'microscope @ git+https://github.com/juliomateoslangerak/microscope.git@pre-production',
         'numpy',
         'pyserial',
         'scipy',
